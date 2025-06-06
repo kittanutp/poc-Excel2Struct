4d76d328802ef6f3271b95d462001b8bbb1b3bf5
Kittanut Pongjirapat <104662057+kittanutp@users.noreply.github.com>
2025-06-06 12:24:46 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"time"
-
 	excel2structpoc "github.com/kittanutp/playground/excel-2-struct-poc"
 )
 
-type Data struct {
-	Term           int       `json:"term"`
-	SubAccount     string    `json:"sub_account"`
-	CompanyName    string    `json:"company_name"`
-	IsAffiliation  bool      `json:"is_affiliation"`
-	PaymentDueDate time.Time `json:"payment_due_date"`
-}
 
 func main() {
 	excel2structpoc.Run()
